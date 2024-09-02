0dee66fe8437163f0a6553e494029063e527c2a2
Chiryasova Ekaterina <kat.rok@mail.ru>
2024-09-02 21:02:55 +0300

    Добавлены  файлы

---
+++ src/main/java/autoTest/Player.java
@@ -0,0 +1,21 @@
+
+package autoTest;
+
+import java.util.Random;
+
+public class Player {
+
+    private Random random;
+
+    public Player() {
+        this.random = new Random();
+    }
+
+    public int choiceDoor() {
+        return random.nextInt(3);
+    }
+
+    public int choiceNewDoor(int secondChoiceDoor, int firstChoiceDoor) {
+        return 3 - secondChoiceDoor - firstChoiceDoor;
+    }
+}
